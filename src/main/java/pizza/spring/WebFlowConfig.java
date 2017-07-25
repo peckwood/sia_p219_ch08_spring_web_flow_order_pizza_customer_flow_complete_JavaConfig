3540d6c1d2d94877d299c62ae10b2971016e93fd
raiden <peckwood@users.noreply.github.com>
2017-07-25 11:39:59 +0800

    now works for JavaConfig

--- src/main/java/pizza/spring/WebFlowConfig.java
+++ src/main/java/pizza/spring/WebFlowConfig.java
@@ -1,17 +1,15 @@
 package pizza.spring;
 
-import java.util.Collections;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.webflow.config.AbstractFlowConfiguration;
 import org.springframework.webflow.definition.registry.FlowDefinitionRegistry;
 import org.springframework.webflow.engine.builder.support.FlowBuilderServices;
 import org.springframework.webflow.executor.FlowExecutor;
-import org.springframework.webflow.mvc.builder.MvcViewFactoryCreator;
+import org.springframework.webflow.mvc.servlet.FlowHandlerAdapter;
+import org.springframework.webflow.mvc.servlet.FlowHandlerMapping;
 
 @Configuration
 @ComponentScan({"pizza"})
@@ -40,16 +38,25 @@ public class WebFlowConfig extends AbstractFlowConfiguration {
 	@Bean
 	public FlowBuilderServices flowBuilderServices() {
 		return getFlowBuilderServicesBuilder()
-				.setViewFactoryCreator(mvcViewFactoryCreator())
 				.setDevelopmentMode(true)
 				.build();
 	}
-
-	@Bean
-	public MvcViewFactoryCreator mvcViewFactoryCreator() {
-		MvcViewFactoryCreator factoryCreator = new MvcViewFactoryCreator();
-		factoryCreator.setViewResolvers(Collections.singletonList(this.webMvcConfig.viewResolver()));
-		factoryCreator.setUseSpringBeanBinding(true);
-		return factoryCreator;
+	
+	@Bean 
+	@Autowired
+	public FlowHandlerAdapter flowHandlerAdapter(FlowExecutor flowExecutor) {
+	    FlowHandlerAdapter flowHandlerAdapter = new FlowHandlerAdapter();
+	    flowHandlerAdapter.setFlowExecutor(flowExecutor);
+	    return flowHandlerAdapter;
 	}
+
+	@Bean 
+	@Autowired
+	public FlowHandlerMapping flowHandlerMapping(FlowDefinitionRegistry flowDefinitionRegistry) {
+	    FlowHandlerMapping flowHandlerMapping = new FlowHandlerMapping();
+	    flowHandlerMapping.setFlowRegistry(flowDefinitionRegistry);
+	    flowHandlerMapping.setOrder(0);
+	    return flowHandlerMapping;
+	}
+
 }
